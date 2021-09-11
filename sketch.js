24b0f9b29a2d5d1cca13bb3c83ae31f801970629
alunity <75143943+alunity@users.noreply.github.com>
2021-09-11 23:11:46 +0100

    added sliders to change Dices and Throws dynamically

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
-THROWS = 1000000;
-DICES = 20;
+// Defaults
+const THROWS = 10000;
+const DICES = 50;
+
+// Sliders
+let throws = 10000;
+let dices = 50;
 
 function dice() {
   return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
@@ -7,6 +12,11 @@ function dice() {
 
 class normalDistro {
   constructor(throws, dices) {
+    this.init(throws, dices);
+    console.log(this.results);
+  }
+
+  init(throws, dices) {
     this.throws = throws;
     this.dices = dices;
     this.results = this.simulateDiceThrows();
@@ -43,7 +53,9 @@ class normalDistro {
   }
 
   display() {
+    fill(230);
     rect(width / 10, height / 10, (width / 10) * 8, (height / 10) * 8);
+    fill("cyan");
     for (let i = 0; i < Object.keys(this.results).length; i++) {
       let number = Object.keys(this.results)[i];
       let heightOfBar = this.dimensions[1] * this.results[number];
@@ -59,13 +71,33 @@ class normalDistro {
 let app;
 
 function setup() {
-  createCanvas(1000, 1000);
+  var canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.8);
+  canvas.parent("sketch-holder");
+
   app = new normalDistro(THROWS, DICES);
 }
 
 function draw() {
-  background(220);
+  background(255);
   app.display();
 }
 
+function updateThrows(e) {
+  throws = e.target.value;
+  // console.log(e.target.value);
+  updateGraph();
+}
+
+function updateDices(e) {
+  dices = e.target.value;
+  // console.log(e.target.value);
+  updateGraph();
+}
+
+function updateGraph() {
+  app.init(throws, dices);
+}
+
+document.getElementById("throws").addEventListener("input", updateThrows);
+document.getElementById("dices").addEventListener("input", updateDices);
 // console.log(simulateDiceThrows(THROWS, DICES))
